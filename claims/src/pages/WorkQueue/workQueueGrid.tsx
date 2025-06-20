c276610a5388513a3c11c622784c60c7b99c6413
Harish.K <kollatiharish@gmail.com>
2025-06-20 13:46:18 +0530

    fixed the issue with imports

--- claims/src/pages/WorkQueue/workQueueGrid.tsx
+++ claims/src/pages/WorkQueue/workQueueGrid.tsx
@@ -1,7 +1,7 @@
 // components/WorkQueueTableGrid.tsx
 
 import React from "react";
-import DynamicTable from "../../Components/reusable/dynamicTable"
+import DynamicTable from "../../components/reusable/dynamicTable"
 import { Chip, Stack, Typography } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 
